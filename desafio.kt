3bda55f99b7a80b1a9f91407e0ab4f68a4564267
Gabriel Gomes Pena <86542760+GabrielGPena793@users.noreply.github.com>
2023-08-22 00:05:07 -0300

    challange solution

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, val nivel: Nivel, vararg conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        if (inscritos.contains(usuario)) {
+            throw Error("Usuário já exite.")
+        }
+        
+        inscritos.add(usuario)
     }
+    
+    fun alunosMatriculados() {
+        if (inscritos.isEmpty()) {
+            throw Error("Não há usuários cadastrados ainda.")
+        }
+        
+        for (aluno in inscritos) {
+        	println(aluno.nome)
+        }
+    }
+ 
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Usuarios
+    
+    val userJorge = Usuario("Jorge", "jorge@hotmail.com")
+    val userClaudia = Usuario("Claudia", "claudia@hotmail.com")
+    val userLisangela = Usuario("Lisangela", "lisangela@hotmail.com")
+    
+    // Conteúdos
+    
+    val conteudo1 = ConteudoEducacional("Pratica com kotlin", 30)
+    val conteudo2 = ConteudoEducacional("Coleções", 120)
+    val conteudo3 = ConteudoEducacional("Variáveis e tipos", 100)
+    val conteudo4 = ConteudoEducacional("Funções", 200)
+    
+    // Formação
+    val kotlinCourse = Formacao(
+        nome = "Koltin", 
+        nivel = Nivel.INTERMEDIARIO, 
+        listOf(conteudo1, conteudo2, conteudo3, conteudo4)
+    )
+    
+    // Add usuários
+    
+
+    kotlinCourse.matricular(userJorge)
+    kotlinCourse.matricular(userClaudia)
+    kotlinCourse.matricular(userLisangela)
+   
+   
+   	kotlinCourse.alunosMatriculados()
+    
 }
